d11efde2e4774a756c991daca4ca8185d5c777ba
lc-sanchez <csanchez_98@outlook.com>
2023-05-20 15:31:20 -0300

    fixing lints problems

--- src/components/grupos/grupo1/components/common/Title.jsx
+++ src/components/grupos/grupo1/components/common/Title.jsx
@@ -2,11 +2,10 @@ import * as React from 'react';
 import PropTypes from 'prop-types';
 import Typography from '@mui/material/Typography';
 
-
 const Title = (props) => {
   <Typography component="h2" variant="h6" color="primary" gutterBottom>
     {props.children}
-  </Typography>
+  </Typography>;
 };
 
 Title.propTypes = {
--- src/components/grupos/grupo1/components/common/Tooltip.jsx
+++ src/components/grupos/grupo1/components/common/Tooltip.jsx
@@ -1,11 +1,13 @@
-import { Tooltip } from "@mui/material";
+/* eslint-disable react/prop-types */
+import { Tooltip } from '@mui/material';
 
-import React from 'react'
+import React from 'react';
 
-export default function TooltipCus({icon, title}) {
-    return (
+// eslint-disable-next-line react/function-component-definition
+export default function TooltipCus({ icon, title }) {
+  return (
     <Tooltip title={title} placement="right">
-        {icon}
+      {icon}
     </Tooltip>
-    )
+  );
 }
