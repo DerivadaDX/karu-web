e925d236130b9e57a7e6f7eae3b06593ffc9928e
DerivadaDX <derivadadx@gmail.com>
2023-05-21 00:13:08 -0300

    Se creó http-service.js.

---
+++ src/components/grupos/grupo2/services/http-service.js
@@ -0,0 +1,15 @@
+import axios from 'axios';
+
+const baseUrl = process.env.REACT_APP_G2_URL_BACK || 'http://localhost:8000';
+const version = '/v1';
+const urlCompleta = baseUrl + version;
+
+const request = {
+  get: (url, params) => axios.get(urlCompleta + url, { params }),
+  post: (url, params) => axios.post(urlCompleta + url, params),
+  put: (url, params) => axios.post(urlCompleta + url, params),
+  patch: (url, params) => axios.post(urlCompleta + url, params),
+  delete: (url, params) => axios.post(urlCompleta + url, { data: params }),
+};
+
+export default request;
