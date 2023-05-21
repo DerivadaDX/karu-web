1b19e0492e0f22c4a14d3ed3013df7f9547b1a41
DerivadaDX <derivadadx@gmail.com>
2023-05-21 18:00:20 -0300

    Se movió http-service.js a raíz de grupo2

--- src/components/grupos/grupo2/services/sucursal-service.js
+++ src/components/grupos/grupo2/services/sucursal-service.js
@@ -1,4 +1,4 @@
-import HttpService from './http-service';
+import HttpService from '../http-service';
 
 const SucursalService = {
   obtenerSucursalPorId: (idSucursal) => HttpService.get(`/sucursal/${idSucursal}/`),
