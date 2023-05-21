99b7832bdab6ca7e2393252fd8bf79584ccf4d87
Maite <maitena_escalada@hotmail.com>
2023-05-21 00:34:54 -0300

    add post del json con la evaluacion

--- src/components/grupos/grupo1/services/services-checklist.js
+++ src/components/grupos/grupo1/services/services-checklist.js
@@ -1,10 +1,23 @@
+/* eslint-disable no-console */
 import axios from 'axios';
+import evaluacion from '../pages/checklist-evaluacion/evaluacion.json';
 
 const url = 'https://autotech2.onrender.com/evaluaciones';
 
 const getChecklistEvaluaciones = async () => axios.get(`${url}/checklist/listar/`);
 
+const postRegistroEvaluaciones = async () => axios({
+  method: 'post',
+  url: `${url}/registros/crear/`,
+  data: {
+    id_turno: evaluacion.id_turno,
+    id_task_puntaje: evaluacion.id_task_puntaje,
+    detalle: evaluacion.detalle,
+  },
+});
+
 export {
   // eslint-disable-next-line import/prefer-default-export
   getChecklistEvaluaciones,
+  postRegistroEvaluaciones,
 };
