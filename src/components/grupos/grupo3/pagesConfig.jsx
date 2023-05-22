4f4341f9030c683906deaba5f4e875989f8705d0
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-05-22 15:11:09 -0300

    agrego llamado al componente nueva pagina

--- src/components/grupos/grupo3/pagesConfig.jsx
+++ src/components/grupos/grupo3/pagesConfig.jsx
@@ -1,17 +1,10 @@
-import { Home } from '@mui/icons-material';
 import DriveEtaIcon from '@mui/icons-material/DriveEta';
 
 import React from 'react';
 import Cotizar from './components/cotizar/Cotizar';
+import NuevaPagina from './components/cotizar/NuevaPagina';
 
 const GROUP_3_PAGES_CONFIG = [
-  {
-    id: 'g3-Home',
-    name: 'Home',
-    href: '/home',
-    icon: <Home />,
-    page: null,
-  },
   {
     id: 'g3-Cotizar vehiculos',
     name: 'Cotizar vehiculos',
