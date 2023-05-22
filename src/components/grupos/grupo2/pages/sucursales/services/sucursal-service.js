2331c7de2a8d989faad39ffd15e7a38b27bea1fa
Iván <derivadadx@gmail.com>
2023-05-22 19:21:53 -0300

    [AB#110] Implementación rehabilitar sucursal (#24)
    - Se renombró `PopUpDeshabilitarSucursal` a `PopUpCambiarEstadoSucursal`.
    - Se modificó `PopUpCambiarEstadoSucursal` para que habilite o deshabilite la sucursal en función de su propiedad `activa`.
    
    Fixes AB#110.

--- src/components/grupos/grupo2/pages/sucursales/services/sucursal-service.js
+++ src/components/grupos/grupo2/pages/sucursales/services/sucursal-service.js
@@ -4,6 +4,7 @@ const SucursalService = {
   obtenerSucursales: () => HttpService.get('/sucursales/'),
   crearSucursal: (data) => HttpService.post('/sucursal/', data),
   deshabilitarSucursal: (idSucursal) => HttpService.delete(`/sucursal/${idSucursal}/`),
+  habilitarSucursal: (idSucursal) => HttpService.put(`/sucursal/${idSucursal}/`, { activa: true }),
 };
 
 export default SucursalService;
