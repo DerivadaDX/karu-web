7f065b5eb321f3b284d51c7494bec31135bdda4d
DerivadaDX <derivadadx@gmail.com>
2023-05-23 22:17:01 -0300

    Se cambió tabla por MaterialReactTable.

--- src/components/grupos/grupo2/pages/sucursales/components/ListadoSucursales.jsx
+++ src/components/grupos/grupo2/pages/sucursales/components/ListadoSucursales.jsx
@@ -1,214 +1,111 @@
-import React, { useEffect, useState } from 'react';
+import React, { useEffect, useMemo, useState } from 'react';
 
 import {
-  Box,
-  Divider,
-  InputLabel,
-  MenuItem,
-  Select,
-  Table,
-  TableBody,
-  TableCell,
-  TableHead,
-  TableRow,
-  TextField,
+  Box, Typography,
 } from '@mui/material';
 import CheckBoxOutlinedIcon from '@mui/icons-material/CheckBoxOutlined';
 import CheckBoxOutlineBlankOutlinedIcon from '@mui/icons-material/CheckBoxOutlineBlankOutlined';
+import MaterialReactTable from 'material-react-table';
 
 import SucursalService from '../services/sucursal-service';
-import PopUpCambiarEstadoSucursal from './PopUpCambiarEstadoSucursal';
-import CrearSucursal from './CrearSucursal';
 
-const styles = {
-  barraSuperior: {
-    display: 'flex',
-    alignItems: 'center',
-    '& > :not(style)': { m: 1 },
-  },
-  divider: {
-    bgcolor: 'text.primary',
-  },
-  header: {
-    marginTop: 20,
-  },
+const renderCheckTieneTaller = ({ row }) => (
+  row.original.posee_taller
+    ? <CheckBoxOutlinedIcon />
+    : <CheckBoxOutlineBlankOutlinedIcon />
+);
+
+const renderEstadoSucursal = ({ row }) => {
+  const sucursalActiva = row.original.activa;
+
+  return (
+    <Typography
+      variant="body1"
+      component="span"
+      sx={{ fontWeight: 'bold' }}
+      color={(theme) => (sucursalActiva ? theme.palette.success.main : theme.palette.error.main)}
+    >
+      {sucursalActiva ? 'Hab.' : 'Deshab.'}
+    </Typography>
+  );
 };
 
 const ListadoSucursales = () => {
   const [sucursales, setSucursales] = useState([]);
-  const [records, setRecords] = useState([]);
-
-  const [valorCategoriaAFiltrar, setValorCategoriaAFiltrar] = useState('id');
-  const [valorFiltroPoseeTaller, setValorFiltroPoseeTaller] = useState('todas');
-  const [valorFiltroEstado, setValorFiltroEstado] = useState('todas');
+  const [cargando, setCargando] = useState(true);
 
   const obtenerSucursales = () => {
     SucursalService.obtenerSucursales()
       .then((response) => {
-        const sucursalesOrdenadasPorIdAsc = response.data.sort((a, b) => a.id - b.id);
-
-        setSucursales(sucursalesOrdenadasPorIdAsc);
-        setRecords(sucursalesOrdenadasPorIdAsc);
+        setSucursales(response.data);
+        setCargando(false);
       });
   };
 
-  const filtrarPorCategoria = (event) => {
-    const options = {
-      id: 'id',
-      nombre: 'nombre',
-      provincia: 'provincia',
-      localidad: 'localidad',
-      calle: 'calle',
-    };
-
-    const prop = options[valorCategoriaAFiltrar] || 'id';
-
-    if (prop === 'id') {
-      setRecords(sucursales.filter((f) => f[prop].toString().includes(event.target.value)));
-    } else {
-      const valueToSearch = event.target.value.toLowerCase();
-      setRecords(sucursales.filter((f) => f[prop].toLowerCase().includes(valueToSearch)));
-    }
-  };
-
-  const filtrarPorPoseeTaller = (record) => {
-    if (valorFiltroPoseeTaller === 'si') {
-      return record.posee_taller === true;
-    }
-
-    if (valorFiltroPoseeTaller === 'no') {
-      return record.posee_taller === false;
-    }
-
-    return true;
-  };
-
-  const filtrarPorEstado = (record) => {
-    if (valorFiltroEstado === 'si') {
-      return record.activa === true;
-    }
-
-    if (valorFiltroEstado === 'no') {
-      return record.activa === false;
-    }
-
-    return true;
-  };
-
-  const actualizarEstadoDeSucursal = (id) => {
-    const cambiarEstadoDeSucursal = (sucursal) => {
-      const esLaSucursalActualizada = sucursal.id === id;
-      const retVal = esLaSucursalActualizada
-        ? { ...sucursal, activa: !sucursal.activa }
-        : sucursal;
-
-      return retVal;
-    };
-
-    setRecords((prevRecords) => prevRecords.map(cambiarEstadoDeSucursal));
-  };
-
-  const recordsFiltrados = records
-    .filter(filtrarPorPoseeTaller)
-    .filter(filtrarPorEstado);
-
   useEffect(obtenerSucursales, []);
+
+  const columnas = useMemo(
+    () => [
+      {
+        accessorKey: 'id',
+        header: 'ID',
+      },
+      {
+        accessorKey: 'nombre',
+        header: 'Nombre',
+      },
+      {
+        accessorKey: 'provincia',
+        header: 'Provincia',
+      },
+      {
+        accessorKey: 'localidad',
+        header: 'Localidad',
+      },
+      {
+        accessorKey: 'codigo_postal',
+        header: 'C.P.',
+      },
+      {
+        accessorKey: 'calle',
+        header: 'Calle',
+      },
+      {
+        accessorKey: 'numero',
+        header: 'Altura',
+      },
+      {
+        accessorKey: 'posee_taller',
+        header: 'Taller',
+        Cell: renderCheckTieneTaller,
+      },
+      {
+        accessorKey: 'activa',
+        header: 'Estado',
+        Cell: renderEstadoSucursal,
+      },
+    ],
+    [],
+  );
 
   return (
     <Box>
-      <Box sx={styles.barraSuperior}>
-        <InputLabel>Seleccionar categoría:</InputLabel>
-        <Select
-          value={valorCategoriaAFiltrar}
-          onChange={(event) => setValorCategoriaAFiltrar(event.target.value)}
-        >
-          <MenuItem value="id">ID</MenuItem>
-          <MenuItem value="nombre">Nombre</MenuItem>
-          <MenuItem value="provincia">Provincia</MenuItem>
-          <MenuItem value="localidad">Localidad</MenuItem>
-          <MenuItem value="calle">Calle</MenuItem>
-        </Select>
-
-        <TextField
-          label="Buscar..."
-          variant="outlined"
-          onChange={filtrarPorCategoria}
-        />
-
-        <Divider orientation="vertical" variant="middle" sx={styles.divider} flexItem />
-
-        <InputLabel>Posee taller:</InputLabel>
-        <Select
-          value={valorFiltroPoseeTaller}
-          onChange={(event) => setValorFiltroPoseeTaller(event.target.value)}
-        >
-          <MenuItem value="todas">Todas</MenuItem>
-          <MenuItem value="si">Sí</MenuItem>
-          <MenuItem value="no">No</MenuItem>
-        </Select>
-
-        <Divider orientation="vertical" variant="middle" sx={styles.divider} flexItem />
-
-        <InputLabel>Estado:</InputLabel>
-        <Select
-          value={valorFiltroEstado}
-          onChange={(event) => setValorFiltroEstado(event.target.value)}
-        >
-          <MenuItem value="todas">Todas</MenuItem>
-          <MenuItem value="si">Habilitadas</MenuItem>
-          <MenuItem value="no">Deshabilitadas</MenuItem>
-        </Select>
-
-        <Divider orientation="vertical" variant="middle" sx={styles.divider} flexItem />
-
-        <CrearSucursal onCreate={obtenerSucursales} />
-      </Box>
-
-      <Divider sx={styles.divider} />
-
-      <Table size="small" style={styles.header}>
-        <TableHead>
-          <TableRow>
-            <TableCell>ID</TableCell>
-            <TableCell>Nombre</TableCell>
-            <TableCell>Provincia</TableCell>
-            <TableCell>Localidad</TableCell>
-            <TableCell>C.P</TableCell>
-            <TableCell>Calle</TableCell>
-            <TableCell>Altura</TableCell>
-            <TableCell>Taller</TableCell>
-            <TableCell>Estado</TableCell>
-            <TableCell> </TableCell>
-          </TableRow>
-        </TableHead>
-        <TableBody>
-          {recordsFiltrados.map((sucursal) => (
-            <TableRow key={sucursal.id}>
-              <TableCell>{sucursal.id}</TableCell>
-              <TableCell>{sucursal.nombre}</TableCell>
-              <TableCell>{sucursal.provincia}</TableCell>
-              <TableCell>{sucursal.localidad}</TableCell>
-              <TableCell>{sucursal.codigo_postal}</TableCell>
-              <TableCell>{sucursal.calle}</TableCell>
-              <TableCell>{sucursal.numero}</TableCell>
-              <TableCell>
-                {sucursal.posee_taller
-                  ? <CheckBoxOutlinedIcon />
-                  : <CheckBoxOutlineBlankOutlinedIcon />}
-              </TableCell>
-              <TableCell style={{ color: sucursal.activa ? 'green' : 'red' }}>
-                {sucursal.activa ? 'Hab.' : 'Deshab.'}
-              </TableCell>
-              <TableCell>
-                <PopUpCambiarEstadoSucursal
-                  sucursal={sucursal}
-                  onSuccess={actualizarEstadoDeSucursal}
-                />
-              </TableCell>
-            </TableRow>
-          ))}
-        </TableBody>
-      </Table>
+      <MaterialReactTable
+        columns={columnas}
+        data={sucursales}
+        state={{ isLoading: cargando }}
+        positionActionsColumn="last"
+        defaultColumn={{ minSize: 10, maxSize: 100 }}
+        muiTopToolbarProps={{
+          sx: {
+            display: 'flex',
+            flexWrap: 'inherit',
+            justifyContent: 'flex-end',
+            overflow: 'auto',
+            maxHeight: '200px',
+          },
+        }}
+      />
     </Box>
   );
 };
