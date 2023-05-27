aebcd671ad9416b32348bacf7a0624dcb7911bee
Cris <66685602+CrisArquimediano@users.noreply.github.com>
2023-05-27 17:56:18 -0300

    Funcionalidad de dar Alta turno de Reparación para Venta. (#42)
    * Agregando services para turnos no validos y turnos cancelados
    
    * Agregando tablas de turnos cancelados y turnos no validos. Modificando Detalle turno para que tome estado ausente
    
    * Borrando filaDetalle de las tablas en visualizar agenda
    
    * Agregando columna estado a la tabla de turnos no validos
    
    * agregando que se vea id de turno en vista tecnicos y eliminado columna nombre tecnico en tablas en proceso y terminados
    
    * Agregando boton reprogramar en tabla cancelados
    
    * add checklist de reparaciones y modifique pagesConfig para visualizar la pagina
    
    * add modelo de json para reparacion
    
    * actualizando pagesConfic agregando services
    
    * Avanzando tabla de services
    
    * Modificando celda activacion
    
    * modificando nombre carpeta services a visualizar-services
    
    * Agregando services y avanzando tabla de services
    
    * add reprogramacion turno, le puse calendario, select de horas, botones y guardo las cosas en un json
    
    * add popup de reprogramacion en "cancelados"
    
    * Completando UI de Turnos Reparaciones para venta.
    
    * Separando componente Talleres y setenado taller seleccionado.
    
    * Agregando Helpers, y creando msj de Alert al validar patente.
    
    * Agregando PopUp. Queda mandar al back los datos y crear el turno.
    
    * Renombro el componente de turno reparación para venta.
    
    * Acomodando estilo de Turno reparación para venta.
    
    * Separando en turno cliente service de evaluación.
    
    * Refactor de Turnos cliente, ahora solo para Service.
    
    * Agregando campo DNI al form de turno cliente.
    
    * Tomando la disponibilidad con el nuevo endpoint para reparación.
    
    * Terminando Alta de turno de Reparación para Venta.
    
    ---------
    
    Co-authored-by: lc-sanchez <csanchez_98@outlook.com>
    Co-authored-by: Maite <maitena_escalada@hotmail.com>

---
+++ src/components/grupos/grupo1/pages/turnos/Componentes/FechasHorarios.jsx
@@ -0,0 +1,150 @@
+/* eslint-disable react/prop-types */
+import React from 'react';
+import {
+  Box, Grid, FormControl, Select, InputLabel, MenuItem,
+} from '@mui/material';
+import { AdapterDayjs } from '@mui/x-date-pickers/AdapterDayjs';
+import { LocalizationProvider } from '@mui/x-date-pickers/LocalizationProvider';
+import { MobileDatePicker } from '@mui/x-date-pickers/MobileDatePicker';
+import dayjs from 'dayjs';
+import 'dayjs/locale/es';
+import { format } from 'date-fns';
+import utc from 'dayjs/plugin/utc';
+import timezone from 'dayjs/plugin/timezone';
+
+import axios from 'axios';
+import feriados from '../turno-cliente/feriados.json';
+import disponibilidad from '../turno-cliente/disponibilidad.json';
+import turno from '../turno.json';
+
+dayjs.extend(utc);
+dayjs.extend(timezone);
+dayjs.locale('es');
+dayjs.tz.setDefault('America/Argentina/Buenos_Aires');
+
+const today = dayjs();
+const tomorrow = dayjs().add(1, 'day');
+const limite = dayjs().add(31, 'day');
+
+// Para traer la disponibilidad de un taller
+
+const fetchAgendaData = async (endPoint, msjError) => {
+  try {
+    const response = await axios.get(endPoint);
+    // eslint-disable-next-line no-import-assign
+    disponibilidad = response.data;
+    msjError('');
+  } catch (error) {
+    msjError(error.response.data);
+  }
+};
+
+const isFeriadoIsMas30Dias = (date) => {
+  const actual = format(new Date(date), 'dd/MM/yyyy');
+  const hoy = format(new Date(today), 'dd/MM/yyyy');
+  let isFeriado = false;
+
+  // eslint-disable-next-line no-restricted-syntax
+  for (const dia in feriados) { if (actual === feriados[dia]) { isFeriado = true; } }
+  return isFeriado || date > limite || actual === hoy;
+};
+
+const Disponibilidad = ({
+  endPoint, setFecha, setHora, msjError,
+}) => {
+  const [dia, setDia] = React.useState(tomorrow);
+
+  fetchAgendaData(endPoint, msjError);
+
+  return (
+    <LocalizationProvider dateAdapter={AdapterDayjs}>
+      <Box
+        sx={{
+          marginTop: 2,
+          marginBottom: 1,
+          display: 'flex',
+          flexDirection: 'column',
+        }}
+      >
+        <MobileDatePicker
+          margin="normal"
+          fullWidth
+          required
+          label="Fecha"
+          format="DD/MM/YYYY"
+          defaultValue={tomorrow}
+          disablePast
+          shouldDisableDate={isFeriadoIsMas30Dias}
+          views={['year', 'month', 'day']}
+          value={dia}
+          onChange={(newValue) => {
+            setDia(newValue);
+            setFecha(format(new Date(newValue), 'yyyy-MM-dd'));
+            turno.fecha_inicio = format(new Date(newValue), 'yyyy-MM-dd');
+            turno.fecha_fin = format(new Date(newValue), 'yyyy-MM-dd');
+          }}
+        />
+        <Grid item xs={12} md={10}>
+          {turno.fecha_inicio !== ''
+            && (
+              <Box
+                sx={{
+                  marginTop: 3,
+                  marginBottom: 1,
+                  display: 'flex',
+                  flexDirection: 'column',
+                }}
+              >
+                <Hora
+                  required
+                  fecha={turno.fecha_inicio}
+                  diasYhorarios={disponibilidad.dias_y_horarios}
+                  setHoraSeleccionada={setHora}
+                />
+              </Box>
+            )}
+        </Grid>
+      </Box>
+    </LocalizationProvider>
+  );
+};
+
+const Hora = ({ diasYhorarios, fecha, setHoraSeleccionada }) => {
+  const [hora, setHora] = React.useState('');
+  let h;
+
+  const handleHoraChange = (event) => {
+    const selectedValue = event.target.value;
+    setHora(selectedValue);
+    setHoraSeleccionada(`${parseInt(selectedValue, 10)}:00:00`);
+    turno.hora_inicio = `${parseInt(selectedValue, 10)}:00:00`;
+    h = parseInt(selectedValue, 10) + 1;
+    turno.hora_fin = `${h}:00:00`;
+  };
+
+  const horariosDisponibles = diasYhorarios
+    ?.find((item) => item.dia === fecha)?.horarios_disponibles;
+
+  return (
+    <FormControl fullWidth>
+      <InputLabel>Horarios Disponibles</InputLabel>
+      <Select
+        required
+        value={hora}
+        onChange={handleHoraChange}
+        width="50px"
+        label="Horarios Disponibles"
+      >
+        <MenuItem value="">Elija una hora, por favor</MenuItem>
+        {horariosDisponibles
+          && horariosDisponibles.map((horaItem) => (
+            <MenuItem key={horaItem} value={horaItem}>
+              {horaItem}
+            </MenuItem>
+          ))}
+      </Select>
+    </FormControl>
+  );
+};
+
+export default Disponibilidad;
