5201f999fc02e7589fc8515c110d221c655c6a02
sofia alcaraz <sofiialcaraz16@gmail.com>
2023-05-30 16:47:49 -0300

    [115-Task]Implementar creacion de Vendedor desde la web

--- src/components/grupos/grupo2/pages/vendedores/components/PopUpCrearVendedor.jsx
+++ src/components/grupos/grupo2/pages/vendedores/components/PopUpCrearVendedor.jsx
@@ -1,4 +1,4 @@
-import React, { useState } from 'react';
+import React, { createRef, useEffect, useState } from 'react';
 
 import {
   Box,
@@ -16,24 +16,36 @@ import PropTypes from 'prop-types';
 import AddIcon from '@mui/icons-material/Add';
 import { IMaskInput } from 'react-imask';
 import { DatePicker } from '@mui/x-date-pickers';
+import { Controller, useForm } from 'react-hook-form';
+import VendedorService from '../services/vendedor-service';
 
-const cuitComponent = React.forwardRef((props, ref) => {
+const CuitComponent = React.forwardRef((props, inputRef) => {
   const { onChange, ...other } = props;
+  const ref = createRef();
+  const [value, setValue] = useState('');
+
+  useEffect(() => {
+    onChange({ target: { name: other.name, value } });
+  }, [value]);
 
   return (
     <IMaskInput
       // eslint-disable-next-line react/jsx-props-no-spreading
       {...other}
-      onChange={() => undefined}
+      ref={ref}
       mask="00-0000000[0]-0"
-      inputRef={ref}
+      inputRef={inputRef}
+      onChange={() => undefined}
+      value={value}
+      onAccept={(newValue) => {
+        setValue(newValue);
+      }}
       overwrite
-      onAccept={(value) => onChange({ target: { name: props.name, value } })}
     />
   );
 });
 
-cuitComponent.propTypes = {
+CuitComponent.propTypes = {
   name: PropTypes.string.isRequired,
   onChange: PropTypes.func.isRequired,
 };
@@ -41,14 +53,30 @@ cuitComponent.propTypes = {
 const PopUpCrearVendedor = () => {
   const [mostrarPopUpCrearVendedor, setMostrarPopUpCrearVendedor] = useState(false);
   const [mostrarPopUpCreacionExitosa, setMostrarPopUpCreacionExitosa] = useState(false);
+  const [datosVendedor, setDatosVendedor] = useState({});
 
-  const [nombre, setNombre] = useState('');
-  const [apellido, setApellido] = useState('');
-  const [cuit, setCuit] = useState('');
-  const [email, setEmail] = useState('');
-  const [fechaDeIngreso, setFechaDeIngreso] = useState(null);
-  const [cuitIsValid, setCuitIsValid] = useState(true);
-  const [fechaDeIngresoIsValid, setfechaDeIngresoIsValid] = useState(false);
+  const {
+    handleSubmit, control, formState: { errors, isValid },
+  } = useForm({
+    mode: 'onBlur',
+    defaultValues: {
+      nombre: '',
+      apellido: '',
+      email: '',
+      cuit: '',
+      fecha: null,
+    },
+  });
+  const onSubmit = async (data) => {
+    console.log(data);
+    setDatosVendedor(data);
+  };
+
+  useEffect(() => {
+    VendedorService.crearVendedor(datosVendedor)
+      .then(() => setMostrarPopUpCreacionExitosa(true))
+      .catch(() => setMostrarPopUpCreacionExitosa(false));
+  }, [datosVendedor]);
 
   const cambiarVisibilidadPopUpCrearVendedor = () => {
     setMostrarPopUpCrearVendedor(!mostrarPopUpCrearVendedor);
@@ -57,12 +85,6 @@ const PopUpCrearVendedor = () => {
   const cambiarVisibilidadPopUpCreacionExitosa = () => {
     setMostrarPopUpCreacionExitosa(false);
     setMostrarPopUpCrearVendedor(false);
-  };
-  const handleValidation = (e) => {
-    setCuit(e.target.value);
-    const cuitRegex = /^(20|23|24|27|30|33|34)(-\d{8}-\d)$/;
-    console.log(cuitRegex.test(e.target.value));
-    setCuitIsValid(cuitRegex.test(e.target.value));
   };
 
   return (
@@ -87,65 +109,122 @@ const PopUpCrearVendedor = () => {
         <Paper>
           <Stack
             component="form"
-            onSubmit={console.log('y')}
+            onSubmit={handleSubmit(onSubmit)}
             sx={{ paddingX: '70px', paddingY: '50px' }}
           >
-            <TextField
-              id="nombre"
-              value={nombre}
-              label="Nombre"
-              onChange={(event) => setNombre(event.target.value)}
-              variant="standard"
-              margin="dense"
-              required
-            />
-            <TextField
-              id="apellido"
-              value={apellido}
-              label="Apellido"
-              onChange={(event) => setApellido(event.target.value)}
-              variant="standard"
-              margin="dense"
-              required
-            />
-            <TextField
-              id="email"
-              value={email}
-              label="Email"
-              type="email"
-              onChange={(event) => setEmail(event.target.value)}
-              variant="standard"
-              margin="dense"
-              required
-            />
-            <TextField
-              type="text"
-              value={cuit}
-              name="cuit"
-              id="cuit"
-              label="cuit"
-              variant="standard"
-              margin="dense"
-              error={!cuitIsValid}
-              onChange={(event) => handleValidation(event)}
-              InputProps={{
-                inputComponent: cuitComponent,
-                minLength: 8,
+            <Controller
+              name="nombre"
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextField
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  error={Boolean(errors.nombre)}
+                  helperText={errors.nombre?.message}
+                  placeholder="Nombre"
+                  variant="standard"
+                  margin="dense"
+                />
+              )}
+              rules={{
+                required: 'El nombre es requerido',
               }}
-              required
+            />
+            <Controller
+              name="apellido"
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextField
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  error={Boolean(errors.apellido)}
+                  helperText={errors.apellido?.message}
+                  placeholder="Apellido"
+                  variant="standard"
+                  margin="dense"
+                />
+              )}
+              rules={{
+                required: 'El apellido es requerido',
+              }}
+            />
+            <Controller
+              name="email"
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <TextField
+                  onChange={onChange}
+                  onBlur={onBlur}
+                  value={value}
+                  error={Boolean(errors.email)}
+                  helperText={errors.email?.message}
+                  placeholder="Email"
+                  variant="standard"
+                  margin="dense"
+                />
+              )}
+              rules={{
+                required: 'El email es requerido',
+                validate: {
+                  matchPattern: (v) => /^\w+([.-]?\w+)*@\w+([.-]?\w+)*(\.\w{2,3})+$/.test(v) || 'Escriba el email correctamente',
+                },
+              }}
+            />
+            <Controller
+              name="cuit"
+              control={control}
+              render={({ field: { onChange, onBlur } }) => (
+                <TextField
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  placeholder="CUIT"
+                  variant="standard"
+                  margin="dense"
+                  error={Boolean(errors.cuit)}
+                  helperText={errors.cuit?.message}
+                  InputProps={{
+                    inputComponent: CuitComponent,
+                    name: 'cuit',
+                  }}
+                />
+              )}
+              rules={{
+                required: 'El cuit es requerido',
+                pattern: {
+                  value: /^(20|23|24|27|30|33|34)(-\d{8}-\d)$/,
+                  message: 'Ingrese una CUIT válida',
+                },
+              }}
+            />
+            <Controller
+              name="fecha"
+              control={control}
+              render={({ field: { onChange, onBlur, value } }) => (
+                <DatePicker
+                  disableFuture
+                  onBlur={onBlur}
+                  onChange={onChange}
+                  value={value}
+                  label="Fecha de Ingreso"
+                  error={errors.fecha}
+                  helperText={errors.fecha ? 'noo' : 'sadasdasd'}
+                  slotProps={{ textField: { variant: 'standard', margin: 'dense' } }}
+
+                />
+              )}
+              rules={{
+                required: 'La fecha es requerida',
+                pattern: {
+                  value: /^(0[1-9]|1[012])[- /.] (0[1-9]|[12][0-9]|3[01])[- /.] (19|20)\d$/,
+                  message: 'Ingrese una fecha válida',
+                },
+              }}
             />
 
-            <DatePicker
-              id="date"
-              value={fechaDeIngreso}
-              label="Fecha de Ingreso"
-              type="date"
-              onChange={(date) => { setFechaDeIngreso(date); setfechaDeIngresoIsValid(true); }}
-              slotProps={{ textField: { variant: 'standard', margin: 'dense' } }}
-
-            />
             <Button
-              disabled={!cuitIsValid && !fechaDeIngresoIsValid}
+              disabled={!isValid}
               type="submit"
               variant="contained"
               sx={{ marginTop: '2ch' }}
