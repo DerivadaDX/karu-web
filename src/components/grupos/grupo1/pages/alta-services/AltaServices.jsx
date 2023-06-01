aee8d3896d2d322e4f8579a21ea3bc55d3b427de
Cris Correa <artudvels@gmail.com>
2023-06-01 14:24:52 -0300

    Poniendo sin datos la checklist en la pantalla.

--- src/components/grupos/grupo1/pages/alta-services/AltaServices.jsx
+++ src/components/grupos/grupo1/pages/alta-services/AltaServices.jsx
@@ -1,9 +1,11 @@
-import React, { useState } from 'react';
-// import axios from 'axios';
+import React, { useState, useEffect } from 'react';
+import axios from 'axios';
 import TextField from '@mui/material/TextField';
 import Button from '@mui/material/Button';
 import CssBaseline from '@mui/material/CssBaseline';
 import { Box, Paper } from '@mui/material';
+import Toolbar from '@mui/material/Toolbar';
+import { DataGrid } from '@mui/x-data-grid';
 import Typography from '@mui/material/Typography';
 import Container from '@mui/material/Container';
 import ValidarKm from '../turnos/Helpers/validar-km';
@@ -31,6 +33,12 @@ const AltaServiceForm = () => {
   const [alertError, setAlertError] = useState('');
   const [alertMensaje, setAlertMensaje] = useState('');
   const [alertTitulo, setAlertTitulo] = useState('');
+
+  // Para traer los datos de la CHECKLIST
+  const [checklistData, setChecklistData] = useState('');
+  // Debería tener los ítems seleccionados de la checklist
+  const [selectedItems, setSelectedItems] = useState(null);
+  // Fin de datos para CHECKLIST
 
   const guardarKilometraje = (e) => {
     const val = e.target.value;
@@ -115,6 +123,69 @@ const AltaServiceForm = () => {
     }
   };
 
+  // Para la CHECKLIST
+  // eslint-disable-next-line consistent-return
+  const fetchChecklistData = async () => {
+    try {
+      const response = await axios.get(
+        'https://autotech2.onrender.com/service/listar/checklist/',
+      );
+      return response.data;
+    } catch (error) {
+      console.error(error);
+    }
+  };
+
+  useEffect(() => {
+    // Fetch checklist data from API
+    fetchChecklistData()
+      .then((data) => {
+        if (typeof data === 'object' && Array.isArray(data.checklist)) {
+          const rows = data.checklist.map((item) => ({
+            id: item.id_task,
+            elemento: item.elemento,
+            tarea: item.tarea,
+            costo: item.costo_reemplazo,
+            duracion: item.duracion_reemplazo,
+            selected: false,
+          }));
+          setChecklistData(rows);
+        } else {
+          console.error('Invalid tecnicos data format:', data);
+        }
+      })
+      .catch((error) => console.error(error));
+  }, []);
+
+  const handleRowSelected = (rowData) => {
+    const selectedItemId = rowData.row.id;
+    const selItem = checklistData.find(
+      (item) => item.id === selectedItemId,
+    );
+    setSelectedItems(selItem);
+  };
+
+  const guardarItemsSeleccionados = () => {
+    // Acá tengo que ver para pasar una lista o un json de todos los ítems seleccionados
+    if (selectedItems) {
+      const idTecnico = selectedItems.id;
+      const urlAsignarTecnico = `https://autotech2.onrender.com/turnos/asignar-tecnico/${idTecnico}/`;
+
+      axios
+        .post(urlAsignarTecnico)
+        .then(() => {
+          console.log('Técnico asignado:', selectedItems.id);
+          setOpenPopupSeleccion(true);
+        });
+      // .catch((error) => {
+      //   setMsjError(error.response.data);
+      // });
+    } else {
+      setOpenPopupNoSeleccion(true);
+    }
+  };
+  // Termina lo de la CHECKLIST
+
   return (
     <Container component="main" maxWidth="sm" sx={{ mb: 4 }}>
       <Paper variant="outlined" sx={{ my: { xs: 3, md: 6 }, p: { xs: 2, md: 3 } }}>
@@ -171,15 +242,6 @@ const AltaServiceForm = () => {
             {!isKmValido && <Alerts alertType="warning" description="Coberturas válidas: de 5000 a 200000 km." title="Kilometraje inválido" />}
             {/* eslint-disable-next-line max-len */}
             {/* {msjError && <Alerts alertType="error" description={msjError} title="No se encontró service." />} */}
-            <Button
-              type="submit"
-              fullWidth
-              variant="contained"
-              color="secondary"
-              sx={{ mt: 3, mb: 2 }}
-            >
-              Crear Service
-            </Button>
           </Box>
           <Popup
             title="Error en datos requeridos."
@@ -227,8 +289,108 @@ const AltaServiceForm = () => {
           </Popup>
         </Box>
       </Paper>
+      {/* ACÁ empieza lo nuevo para agregar la checklist */}
+      <EnhancedTableToolbar titulo="Checklist" />
+      <div style={{ height: 400, width: '100%' }}>
+        <DataGrid
+          rows={checklistData}
+          columns={[
+            { field: 'id', headerName: 'ID', width: 170 },
+            { field: 'nombre', headerName: 'Nombre', width: 380 },
+            { field: 'dni', headerName: 'DNI', width: 230 },
+            { field: 'categoria', headerName: 'Categoría', width: 230 },
+            { field: 'taller', headerName: 'Taller', width: 130 },
+          ]}
+          disableMultipleSelection
+          checkboxSelection={false}
+          onRowClick={(rowData) => handleRowSelected(rowData)}
+          // Set the selected item ID as the selectionModel
+          selectionModel={selectedItems ? [selectedItems.id] : []}
+          pageSize={5}
+        />
+        <Box sx={{ display: 'flex', justifyContent: 'center' }}>
+          <Button
+            variant="outlined"
+            color="primary"
+            sx={{ fontSize: '1em', marginTop: '10px', margin: '10px' }}
+            onClick={() => {
+              guardarItemsSeleccionados();
+            }}
+          >
+            Guardar
+          </Button>
+          <Button
+            variant="outlined"
+            color="error"
+            sx={{ fontSize: '1em', margin: '10px' }}
+          >
+            Cancelar
+          </Button>
+        </Box>
+        {/* eslint-disable-next-line max-len */}
+        {/* msjError && <Alerts alertType="error" description={msjError} title="No se puede asignar." /> */}
+        <Popup
+          title="Error en Asignación"
+          description="No ha seleccionado un técnico. Por favor, seleccione uno antes de terminar con el proceso."
+          openDialog={openPopupNoSeleccion}
+          setOpenDialog={setOpenPopupNoSeleccion}
+        >
+          <Box
+            sx={{ margin: '15px', display: 'flex', justifyContent: 'center' }}
+          >
+            <Button
+              color="error"
+              onClick={() => setOpenPopupNoSeleccion(false)}
+            >
+              Aceptar
+            </Button>
+          </Box>
+        </Popup>
+        <Popup
+          title="Asignación completada"
+          description="La asignación del turno al técnico correspondiente ha sido exitosa."
+          openDialog={openPopupSeleccion}
+          setOpenDialog={setOpenPopupSeleccion}
+        >
+          <Box
+            sx={{ margin: '15px', display: 'flex', justifyContent: 'center' }}
+          >
+            <Button>Aceptar</Button>
+          </Box>
+        </Popup>
+        {/* Hasta acá es lo nuevo para agregar la checklist */}
+        <Button
+          type="submit"
+          fullWidth
+          variant="contained"
+          color="secondary"
+          sx={{ mt: 3, mb: 2 }}
+        >
+          Crear Service
+        </Button>
+      </div>
     </Container>
   );
 };
+
+// eslint-disable-next-line react/prop-types
+const EnhancedTableToolbar = ({ titulo }) => (
+  <Toolbar>
+    <Typography
+      sx={{ flex: '1 1 100%' }}
+      color="inherit"
+      variant="subtitle1"
+      component="div"
+    />
+    <Typography
+      sx={{ flex: '1 0.7 100%', justifyContent: 'center' }}
+      variant="h6"
+      id="tableTitle"
+      component="div"
+    >
+      {titulo}
+    </Typography>
+  </Toolbar>
+);
 
 export default AltaServiceForm;
