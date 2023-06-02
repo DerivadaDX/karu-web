c057fe992df8e6a1b613865b32554f076f86c5a4
DerivadaDX <derivadadx@gmail.com>
2023-06-02 00:52:28 -0300

    Se implementó obtenerSucursalesActivas en SucursalService.

--- src/components/grupos/grupo2/pages/vendedores/services/sucursal-service.js
+++ src/components/grupos/grupo2/pages/vendedores/services/sucursal-service.js
@@ -2,6 +2,8 @@ import HttpService from '../../../http-service';
 
 const SucursalService = {
   obtenerSucursal: (idSucursal) => HttpService.get(`/sucursal/${idSucursal}/`),
+  obtenerSucursalesActivas: () => HttpService.get('/sucursales/')
+    .then((response) => response.data.filter((sucursal) => sucursal.activa)),
 };
 
 export default SucursalService;
