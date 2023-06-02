126a13df2ddb4135d0c8173023f5b091baa48fc5
DerivadaDX <derivadadx@gmail.com>
2023-06-02 00:02:31 -0300

    Se movió método crearVendedor a VendedoresService.

--- src/components/grupos/grupo2/pages/vendedores/services/vendedores-service.js
+++ src/components/grupos/grupo2/pages/vendedores/services/vendedores-service.js
@@ -1,6 +1,7 @@
 import HttpService from '../../../http-service';
 
 const VendedoresService = {
+  crearVendedor: (data) => HttpService.post('/vendedor/', data),
   obtenerVendedoresDeSucursal: (idSucursal) => HttpService.get(`/vendedores/${idSucursal}/`),
   obtenerVendedores: () => HttpService.get('/vendedores/'),
 };
