7073c58f8accc898b9a27a82d9c4b80e6bde21da
DerivadaDX <derivadadx@gmail.com>
2023-06-01 21:39:36 -0300

    Se removió `obtenerSucursal` de `SucursalService` en sucursales.

--- src/components/grupos/grupo2/pages/sucursales/services/sucursal-service.js
+++ src/components/grupos/grupo2/pages/sucursales/services/sucursal-service.js
@@ -5,7 +5,6 @@ const SucursalService = {
   crearSucursal: (datosSucursal) => HttpService.post('/sucursal/', datosSucursal),
   deshabilitarSucursal: (idSucursal) => HttpService.put(`/sucursal/${idSucursal}/`, { activa: false }),
   habilitarSucursal: (idSucursal) => HttpService.put(`/sucursal/${idSucursal}/`, { activa: true }),
-  obtenerSucursal: (idSucursal) => HttpService.get(`/sucursal/${idSucursal}/`),
   modificarSucursal: (idSucursal, datosSucursal) => HttpService.put(`/sucursal/${idSucursal}/`, datosSucursal),
 };
 
