8454c8e9ae6e6dd93d0c68bb5cf979712c8ff237
lc-sanchez <csanchez_98@outlook.com>
2023-06-03 18:58:50 -0300

    Modificando endpoint services

--- src/components/grupos/grupo1/services/services-tecnicos.js
+++ src/components/grupos/grupo1/services/services-tecnicos.js
@@ -4,10 +4,11 @@ const url = 'https://autotech2.onrender.com/tecnicos';
 const urlEvaluacion = 'https://autotech2.onrender.com/evaluaciones/registros';
 const urlReparacion = 'https://autotech2.onrender.com/reparaciones';
 const urlExtraordinario = 'https://autotech2.onrender.com/evaluaciones/registros-extraordinario';
+const urlService = 'https://autotech2.onrender.com/service/listar/registros-pendientes';
 
 const getTurnosEvaluacion = async (tecnicoId) => axios.get(`${urlEvaluacion}/listar/${tecnicoId}/`);
 
-const getTurnosService = async (tecnicoId) => axios.get(`${url}/trabajos-en-proceso-service/${tecnicoId}/`);
+const getTurnosService = async (tecnicoId) => axios.get(`${urlService}/${tecnicoId}/`);
 
 const getTurnosReparacion = async (tecnicoId) => axios.get(`${urlReparacion}/listar-en-proceso/${tecnicoId}/`);
 
