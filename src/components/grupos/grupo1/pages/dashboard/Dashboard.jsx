c39ce7303b0a26f6d488c390fb8f2415bc599c33
lc-sanchez <csanchez_98@outlook.com>
2023-06-07 03:25:53 -0300

    Agregando pagesConfig reportes y sacando turnos en proceso de dashboard tecnico

--- src/components/grupos/grupo1/pages/dashboard/Dashboard.jsx
+++ src/components/grupos/grupo1/pages/dashboard/Dashboard.jsx
@@ -1,15 +1,9 @@
+/* eslint-disable no-unused-vars */
 /* eslint-disable jsx-a11y/iframe-has-title */
 import * as React from 'react';
-import Container from '@mui/material/Container';
-import Grid from '@mui/material/Grid';
-import Paper from '@mui/material/Paper';
-import ArticleIcon from '@mui/icons-material/Article';
-import { Box } from '@mui/material';
+import { Box, Divider } from '@mui/material';
 import Header from '../../components/common/Header';
-import TablaTurnosEnProgreso from '../visualizar-agenda/TablaTurnosEnProgreso';
-import LittleTitle from '../../components/common/LittleTitle';
 
-const idTaller = 'T002';
 const idTecnico = '46';
 
 const Home = () => {
@@ -27,6 +21,7 @@ const Home = () => {
         <Box display="flex">
           <Header titulo="Dashboard" subtitulo="Inicio" />
         </Box>
+        <Divider sx={{ bgcolor: 'rgba(0, 0, 0, 0.7)' }} />
       </Box>
       <Box sx={{
         display: 'flex', justifyContent: 'center', alignItems: 'center', margin: 2,
@@ -39,96 +34,6 @@ const Home = () => {
           style={iframeStyles}
         />
       </Box>
-      <Container maxWidth="xl" sx={{ mb: 2 }}>
-        <Grid container spacing={2}>
-          {/*         <Grid item xs={6} sm={3} md={3} lg={3}>
-          <Paper
-            className="mb-2"
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              height: 200,
-              borderRadius: '15px',
-              borderBottom: '4px solid',
-              borderBottomColor: 'secondary.main',
-            }}
-            elevation={5}
-          >
-            <LittleTitle title="Clientes Atendidos" />
-            <CardContent />
-          </Paper>
-        </Grid>
-        <Grid item xs={6} sm={3} md={3} lg={3}>
-          <Paper
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              height: 200,
-              borderBottom: '4px solid',
-              borderBottomColor: 'secondary.main',
-            }}
-            elevation={5}
-            style={{ borderRadius: 15 }}
-          >
-            <LittleTitle title="Turnos Pendientes" />
-            <CardContent />
-          </Paper>
-        </Grid>
-        <Grid item xs={6} sm={3} md={3} lg={3}>
-          <Paper
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              height: 200,
-              borderBottom: '4px solid',
-              borderBottomColor: 'secondary.main',
-            }}
-            elevation={5}
-            style={{ borderRadius: 15 }}
-          >
-            <LittleTitle title="Evaluaciones Hechas" />
-            <CardContent />
-          </Paper>
-        </Grid>
-        <Grid item xs={6} sm={3} md={3} lg={3}>
-          <Paper
-            sx={{
-              p: 2,
-              display: 'flex',
-              flexDirection: 'column',
-              height: 200,
-              borderBottom: '4px solid',
-              borderBottomColor: 'secondary.main',
-            }}
-            elevation={5}
-            style={{ borderRadius: 15 }}
-          >
-            <LittleTitle title="Tecnicos " />
-            <CardContent />
-          </Paper>
-        </Grid> */}
-
-          <Grid item xs={12}>
-            <Paper
-              sx={{
-                p: 2,
-                display: 'flex',
-                flexDirection: 'column',
-                borderBottom: '4px solid',
-                borderBottomColor: 'secondary.main',
-              }}
-              elevation={5}
-              style={{ borderRadius: 15 }}
-            >
-              <LittleTitle title="Turnos en progreso" icon={<ArticleIcon sx={{ marginRight: '10px', color: 'darkgray' }} />} />
-              <TablaTurnosEnProgreso idTaller={idTaller} />
-            </Paper>
-          </Grid>
-        </Grid>
-      </Container>
     </>
   );
 };
