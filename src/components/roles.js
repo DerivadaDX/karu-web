a1b81f21da1f78639c2595efbcb75763ef7918f6
DerivadaDX <derivadadx@gmail.com>
2023-06-08 23:34:18 -0300

    Se creó roles.js.

---
+++ src/components/roles.js
@@ -0,0 +1,13 @@
+const Roles = Object.freeze({
+  CLIENTE: 'cliente',
+  ADMINISTRACION: 'administración',
+  COMERCIAL: 'comercial',
+  TECNICO: 'técnico',
+  VENDEDOR: 'vendedor',
+  SUPERVISOR_DE_TALLER: 'supervisor de taller',
+  SUPERVISOR_DE_VENTAS: 'supervisar de ventas',
+  GERENTE: 'gerente',
+  ADMIN: 'admin',
+});
+
+export default Roles;
