195fbfe5871dd8a5d2fb8661239e9c942df29bc1
DerivadaDX <derivadadx@gmail.com>
2023-06-10 00:24:48 -0300

    Se agregó propiedad esSubmenu a ItemMenu.

--- src/components/layout/side-bar/components/ItemMenu.jsx
+++ src/components/layout/side-bar/components/ItemMenu.jsx
@@ -9,8 +9,10 @@ import {
 } from '@mui/material';
 import PropTypes from 'prop-types';
 
-const ItemMenu = ({ nombre, icono, href }) => (
-  <ListItemButton href={href}>
+const ItemMenu = ({
+  nombre, icono, href, esSubmenu,
+}) => (
+  <ListItemButton href={href} sx={esSubmenu ? { pl: 3 } : null}>
     <ListItemIcon>
       <Tooltip title={nombre} placement="right">
         <Box component="span">
@@ -26,6 +28,11 @@ ItemMenu.propTypes = {
   nombre: PropTypes.string.isRequired,
   icono: PropTypes.element.isRequired,
   href: PropTypes.string.isRequired,
+  esSubmenu: PropTypes.bool,
+};
+
+ItemMenu.defaultProps = {
+  esSubmenu: false,
 };
 
 export default ItemMenu;
