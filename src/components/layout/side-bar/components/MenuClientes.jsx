c3cccba0e91aa85c0525066da5a18257db201f80
DerivadaDX <derivadadx@gmail.com>
2023-06-10 00:13:21 -0300

    Se obtienen elementos en MenuClientes.

--- src/components/layout/side-bar/components/MenuClientes.jsx
+++ src/components/layout/side-bar/components/MenuClientes.jsx
@@ -1,25 +1,49 @@
 import React from 'react';
 
-import PropTypes from 'prop-types';
+import { List } from '@mui/material';
 
 import ItemMenu from './ItemMenu';
+import GROUP_1_PAGES_CONFIG from '../../../grupos/grupo1/pagesConfig';
+import GROUP_2_PAGES_CONFIG from '../../../grupos/grupo2/pagesConfig';
+import GROUP_3_PAGES_CONFIG from '../../../grupos/grupo3/pagesConfig';
+import GROUP_4_PAGES_CONFIG from '../../../grupos/grupo4/pagesConfig';
+import Roles from '../../../roles';
 
-const MenuClientes = ({ elementos }) => elementos.map((elemento) => (
-  <ItemMenu
-    nombre={elemento.nombre}
-    icono={elemento.icono}
-    href={elemento.href}
-  />
-));
+const MenuClientes = () => {
+  const filtrarElementosSoloUrl = (configuracionElemento) => {
+    const esSoloUrl = configuracionElemento.soloUrl === true;
 
-MenuClientes.propTypes = {
-  elementos: PropTypes.arrayOf(PropTypes.shape({
-    id: PropTypes.string.isRequired,
-    name: PropTypes.string.isRequired,
-    href: PropTypes.string.isRequired,
-    icon: PropTypes.element,
-    page: PropTypes.element.isRequired,
-  })).isRequired,
+    return !esSoloUrl;
+  };
+
+  const filtrarElementosParaCliente = (configuracionElemento) => {
+    if (configuracionElemento.roles === undefined) return false;
+
+    const visibleParaCliente = configuracionElemento.roles.includes(Roles.CLIENTE);
+
+    return visibleParaCliente;
+  };
+
+  const elementos = GROUP_1_PAGES_CONFIG
+    .concat(GROUP_2_PAGES_CONFIG)
+    .concat(GROUP_3_PAGES_CONFIG)
+    .concat(GROUP_4_PAGES_CONFIG)
+    .filter(filtrarElementosSoloUrl)
+    .filter(filtrarElementosParaCliente);
+
+  return (
+    <List>
+      {
+        elementos.map((elemento) => (
+          <ItemMenu
+            nombre={elemento.name}
+            icono={elemento.icon}
+            href={elemento.href}
+          />
+        ))
+      }
+    </List>
+  );
 };
 
 export default MenuClientes;
