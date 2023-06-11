0dc7de7136dea8fb9e23632028a7ef043fd7be68
Cris <66685602+CrisArquimediano@users.noreply.github.com>
2023-06-10 21:57:14 -0300

    Agregando botón y mensaje de carga para consultar la garantía en service. (#102)

--- src/components/grupos/grupo1/pages/turnos/turno-service-cliente/TurnoServiceCliente.jsx
+++ src/components/grupos/grupo1/pages/turnos/turno-service-cliente/TurnoServiceCliente.jsx
@@ -34,6 +34,9 @@ const FormularioCliente = () => {
   const [isKmValido, setIsKmValido] = useState(true);
 
   const [msjGarantia, setMsjGarantia] = useState('');
+  // Para la botón de ver el estado de la garantía
+  const [cargandoGarantia, setCargandoGarantia] = useState(false);
+  const [cargando, setCargando] = useState(false);
 
   const msjTurnoCreado = `Se ha creado el turno de service para la patente ${patenteTurno} con ${kilometros} kilómetros para el día ${fecha} a las ${hora} en el taller ${taller}. Recibirá un mail con los datos mencionados. Por favor, recuerde asistir con cédula verde. Gracias.`;
 
@@ -123,10 +126,15 @@ const FormularioCliente = () => {
   // eslint-disable-next-line consistent-return
   const obtenerMsjGarantia = async () => {
     try {
+      setCargandoGarantia(true);
+      setCargando(true);
       const response = await axios.get(`https://autotech2.onrender.com/garantias/garantia-vigente/${patenteTurno}/${fecha}/${kilometros}/`);
       setMsjGarantia(response.data);
     } catch (error) {
       setMsjError(error.response.data);
+    } finally {
+      setCargando(false);
+      setCargandoGarantia(false);
     }
   };
 
@@ -221,9 +229,30 @@ const FormularioCliente = () => {
             {msjError && (
               <Alerts alertType="error" description={msjError} title="No se encontró service." />
             )}
+            <Button
+              type="button"
+              fullWidth
+              variant="contained"
+              color="primary"
+              disabled={!taller || !patenteTurno || !isPatenteValida || !fecha || !hora
+                || !kilometros || !isKmValido || msjGarantia !== ''}
+              sx={{ mt: 3, mb: 2 }}
+              onClick={obtenerMsjGarantia}
+            >
+              Consultar estado de la garantía
+            </Button>
+
+            {cargando && (
+              <Popup
+                title={<LittleHeader titulo="Procesando datos" />}
+                description="Estamos procesando los datos para saber el estado de la garantía. Por favor, espere un momento..."
+                openDialog={cargandoGarantia}
+                setOpenDialog={setCargandoGarantia}
+              />
+            )}
 
             {taller && patenteTurno && isPatenteValida && fecha && hora && kilometros && isKmValido
-              && obtenerMsjGarantia() && msjError === '' && <Alerts alertType="info" description={msjGarantia} title="Garantía" />}
+              && !cargando && msjGarantia && msjError === '' && <Alerts alertType="info" description={msjGarantia} title="Garantía" />}
 
             <Button
               type="submit"
@@ -237,12 +266,12 @@ const FormularioCliente = () => {
             </Button>
           </Box>
           {loading && (
-          <Popup
-            title={<LittleHeader titulo="Enviando datos" />}
-            description="Estamos procesando los datos para confirmar su turno. Por favor, espere un momento..."
-            openDialog={openPopupCargando}
-            setOpenDialog={setOpenPopupCargando}
-          />
+            <Popup
+              title={<LittleHeader titulo="Enviando datos" />}
+              description="Estamos procesando los datos para confirmar su turno. Por favor, espere un momento..."
+              openDialog={openPopupCargando}
+              setOpenDialog={setOpenPopupCargando}
+            />
           )}
           <Popup
             title={<LittleHeader titulo="Error en datos requeridos" />}
@@ -276,7 +305,6 @@ const FormularioCliente = () => {
               <Button
                 color="secondary"
                 variant="outlined"
-                // onClick={() => setOpenPopupSeleccion(false)}
                 onClick={() => {
                   window.location.href = '/';
                 }}
