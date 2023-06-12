cd5cd5978652568e35d3a4582c2ce4278fc4e928
DerivadaDX <derivadadx@gmail.com>
2023-06-12 01:09:27 -0300

    Se modificó Footer para que muestre BotonLogin o BotonLogout en función de isAuthenticated.

--- src/components/layout/footer/Footer.jsx
+++ src/components/layout/footer/Footer.jsx
@@ -1,13 +1,26 @@
-import React from 'react';
+import React, { useContext, useEffect, useState } from 'react';
+
+import { Box } from '@mui/material';
 
 import Copyright from './components/Copyright';
 import BotonLogin from './components/BotonLogin';
+import BotonLogout from './components/BotonLogout';
+import { UserContext } from '../../grupos/grupo4/context/UsersContext';
 
-const Footer = () => (
-  <>
-    <BotonLogin />
-    <Copyright />
-  </>
-);
+const Footer = () => {
+  const [mostrarLogin, setMostrarLogin] = useState(true);
+  const { isAuthenticated } = useContext(UserContext);
+
+  useEffect(() => {
+    setMostrarLogin(!isAuthenticated);
+  }, [isAuthenticated]);
+
+  return (
+    <Box>
+      {mostrarLogin ? <BotonLogin /> : <BotonLogout />}
+      <Copyright />
+    </Box>
+  );
+};
 
 export default Footer;
