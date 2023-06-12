f8998cae20d55fa90c6a1c315c2d2123a469bee4
DerivadaDX <derivadadx@gmail.com>
2023-06-12 00:48:11 -0300

    Se remueve Link de Copyright.

--- src/components/layout/footer/components/Copyright.jsx
+++ src/components/layout/footer/components/Copyright.jsx
@@ -1,6 +1,6 @@
 import React from 'react';
+
 import Typography from '@mui/material/Typography';
-import { Link } from 'react-router-dom';
 
 const actualYear = new Date().getFullYear();
 const copyrightText = `Copyright © KarU Website ${actualYear}.`;
@@ -8,8 +8,6 @@ const copyrightText = `Copyright © KarU Website ${actualYear}.`;
 const Copyright = () => (
   <Typography variant="body2" color="text.secondary" align="center">
     {copyrightText}
-    <br />
-    <Link to="/login">Acceso empleados</Link>
   </Typography>
 );
 
