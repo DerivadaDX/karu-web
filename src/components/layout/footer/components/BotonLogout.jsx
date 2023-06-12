345c9e47b809968d31592708e32a937dd49877ca
DerivadaDX <derivadadx@gmail.com>
2023-06-12 01:08:48 -0300

    Se implementó BotonLogout.

---
+++ src/components/layout/footer/components/BotonLogout.jsx
@@ -0,0 +1,28 @@
+import React, { useContext } from 'react';
+
+import Typography from '@mui/material/Typography';
+import { UserContext } from '../../../grupos/grupo4/context/UsersContext';
+
+const BotonLogout = () => {
+  const { cookie, logOut } = useContext(UserContext);
+  const informacionUsuario = cookie.get('user');
+
+  const textoCerrarSesion = `Cerrar sesión (${informacionUsuario?.username})`;
+
+  return (
+    <Typography
+      variant="body2"
+      color="text.secondary"
+      align="center"
+      onClick={logOut}
+      sx={{
+        textDecoration: 'underline',
+        cursor: 'pointer',
+      }}
+    >
+      {textoCerrarSesion}
+    </Typography>
+  );
+};
+
+export default BotonLogout;
