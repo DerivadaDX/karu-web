a6f513ad11e7d30e1d99a89f57e1aee88fbb2439
ImLevan <chianesevalentin@gmail.com>
2023-06-12 22:29:28 -0300

    agregado campo category

--- src/components/grupos/grupo4/pages/pricesUpdate/PriceUpdate.jsx
+++ src/components/grupos/grupo4/pages/pricesUpdate/PriceUpdate.jsx
@@ -19,6 +19,7 @@ import {
 import { newMassivePriceInput, newPriceModelInput, sellingPriceOfACarInputs } from '../../dto/priceupdate-props';
 import { GetAllModels } from '../../api/API-methods';
 
+
 const PriceUpdate = () => {
     const [sellingPriceOfACarValues, setSellingPriceOfACarValues] = useState({
         plate: "",
