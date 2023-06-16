651cb99f0f2b9b43db851d98b0897abeb6fe323e
Alanknm1 <alan.knm1@gmail.com>
2023-06-16 15:56:57 -0300

    baseURL changed

--- src/components/grupos/grupo4/api/API-methods.js
+++ src/components/grupos/grupo4/api/API-methods.js
@@ -1,8 +1,8 @@
 /*eslint-disable */
 import axios from 'axios';
 const client = axios.create({
-  baseURL: 'http://localhost:8080/api/v1/',
-  // baseURL: 'https://gadmin-backend-production2.up.railway.app/api/v1',
+  // baseURL: 'http://localhost:8080/api/v1/',
+  baseURL: 'https://gadmin-backend-production2.up.railway.app/api/v1',
 });
 
 export const authLogin = async (user) => {
