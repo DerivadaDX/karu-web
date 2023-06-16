12740e137f9c8e6fdf9691d952a30ca7f7ee7a1e
GabiiF <gabriela.fernandez.92@live.com.ar>
2023-06-16 17:46:16 -0300

    agrego que tenga que elegir si o si el tipo de factura

--- src/components/grupos/grupo3/components/factura/PopUpTipoFacturacion.jsx
+++ src/components/grupos/grupo3/components/factura/PopUpTipoFacturacion.jsx
@@ -15,6 +15,7 @@ import { useNavigate } from 'react-router-dom';
 const PopUpTipoFacturacion = () => {
   const [open, setOpen] = useState(false);
   const [tipoFinanciacion, setTipoFinanciacion] = useState('');
+  const [error, setError] = useState(false); // Estado para controlar el error
   const navigate = useNavigate();
 
   const handleOpenTipo = () => {
@@ -31,8 +32,13 @@ const PopUpTipoFacturacion = () => {
   };
 
   const handleAceptarClick = () => {
-    navigate(`/hacer-factura?tipoFinanciacion=${tipoFinanciacion}`);
-    setOpen(false);
+    if (tipoFinanciacion) {
+      navigate(`/hacer-factura?tipoFinanciacion=${tipoFinanciacion}`);
+      setOpen(true);
+      setError(false);
+    } else {
+      setError(true); // Establecer el estado de error si no se seleccionó ninguna opción
+    }
   };
 
   return (
@@ -46,11 +52,14 @@ const PopUpTipoFacturacion = () => {
             <Select
               value={tipoFinanciacion}
               onChange={handleTipoFinanciacionChange}
+              error={error} // Establecer el estado de error en el componente Select
             >
               <MenuItem value="tarjeta-credito">Tarjeta de crédito</MenuItem>
               <MenuItem value="credito-bancario">Crédito bancario</MenuItem>
             </Select>
           </FormControl>
+          {/* Mostrar mensaje de error si no se seleccionó ninguna opción */}
+          {error && <p style={{ color: 'red' }}>Por favor, seleccione un tipo de financiación.</p>}
         </DialogContent>
         <DialogActions>
           <Button onClick={handleCloseTipo}>Cancelar</Button>
